a5b819c0a1ab285f00ad0f4307d378fe2673ee83
imannaseri <inaseri.20@gmail.com>
2019-10-05 18:36:22 +0330

    تغییر در هاست

--- mypacket/settings.py
+++ mypacket/settings.py
@@ -166,7 +166,7 @@ DEFAULT_FROM_EMAIL = 'inaseri.20@gmail.com'
 SERVER_EMAIL = 'inaseri.20@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'test@gmail.com'
+EMAIL_HOST_USER = 'inaseri.20@gmail.com'
 EMAIL_HOST_PASSWORD = 'iman.3802'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 

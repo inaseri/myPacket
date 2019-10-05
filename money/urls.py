0e96ae9d831f1a93961c49baa46e6345d428a7cf
imannaseri <inaseri.20@gmail.com>
2019-10-05 18:21:40 +0330

    برگرداندن اخرین تغییرات

--- money/urls.py
+++ money/urls.py
@@ -16,10 +16,5 @@ urlpatterns = [
     url(r'transactions/(?P<type>[0-9])/',views.index, name='transactions'),
     url('changeTransaction/(?P<id>[0-9]{1,3})', views.changeTransaction, name='change'),
     url('changeBank/(?P<id>[0-9]{1,3})', views.changeBank, name='changeBank'),
-    url(r'^password_reset/$', views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', views.password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', views.password_reset_complete, name='password_reset_complete'),
 ]
 
